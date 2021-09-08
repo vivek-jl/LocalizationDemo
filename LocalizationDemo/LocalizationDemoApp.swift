985b9956ef00d29c1d9e9e14b3d2339bcd03ab98
Vivek Jayakumar <vivek.l@go-jek.com>
2021-09-08 10:05:04 +0400

    Initial Commit

---
+++ LocalizationDemo/LocalizationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationDemoApp.swift
+//  LocalizationDemo
+//
+//  Created by Vivek Jayakumar on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
